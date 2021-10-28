18a911e63a20ba8566e331b919888b3d7901be99
Hunter Dermott <hbdermott@gmail.com>
2021-10-27 19:07:06 -0700

    Moved to .env. Need to scrub previous commits

--- app/firebase/Firebase.js
+++ app/firebase/Firebase.js
@@ -8,18 +8,15 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 // Initialize Firebase
-
-const firebaseConfig = {
-	apiKey: "AIzaSyAlAXeaFjfr1AU8FFgg4AFioOaf14JDjmQ",
-	authDomain: "novel-42fa8.firebaseapp.com",
-	projectId: "novel-42fa8",
-	storageBucket: "novel-42fa8.appspot.com",
-	messagingSenderId: "56976936170",
-	appId: "1:56976936170:web:604dc74a5e406b52965b93",
-	measurementId: "G-6WNP8D4Y2Y",
-};
-
-const app = initializeApp(firebaseConfig);
+const app = initializeApp({
+	apiKey: process.env.NEXT_PUBLIC_REACT_APP_API_KEY,
+	authDomain: process.env.NEXT_PUBLIC_REACT_APP_AUTH_DOMAIN,
+	projectId: process.env.NEXT_PUBLIC_REACT_APP_PROJECT_ID,
+	storageBucket: process.env.NEXT_PUBLIC_REACT_APP_STORAGE_BUCKET,
+	messagingSenderId: process.env.NEXT_PUBLIC_REACT_APP_SENDER_ID,
+	appId: process.env.NEXT_PUBLIC_REACT_APP_APP_ID,
+	measurementId: process.env.NEXT_PUBLIC_REACT_APP_MEASUREMENT_ID,
+});
 const db = getFirestore(app);
 const auth = getAuth(app);
 
