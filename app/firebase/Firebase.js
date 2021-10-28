65debdfe6b22185c91c67fe90e175a6de2bde8e3
Hunter Dermott <hbdermott@gmail.com>
2021-10-27 19:07:06 -0700

    Moved to .env. Need to scrub previous commits

--- app/firebase/Firebase.js
+++ app/firebase/Firebase.js
@@ -8,18 +8,15 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 // Initialize Firebase
-
-const firebaseConfig = {
-	apiKey: ***REMOVED***,
-	authDomain: ***REMOVED***,
-	projectId: ***REMOVED***,
-	storageBucket: ***REMOVED***,
-	messagingSenderId: ***REMOVED***,
-	appId: ***REMOVED***,
-	measurementId: ***REMOVED***,
-};
-
-const app = initializeApp(firebaseConfig);
+const app = initializeApp({
+	apiKey: process.env.NEXT_PUBLIC_REACT_APP_API_KEY,
+	authDomain: process.env.NEXT_PUBLIC_REACT_APP_AUTH_DOMAIN,
+	projectId: process.env.NEXT_PUBLIC_REACT_APP_PROJECT_ID,
+	storageBucket: process.env.NEXT_PUBLIC_REACT_APP_STORAGE_BUCKET,
+	messagingSenderId: process.env.NEXT_PUBLIC_REACT_APP_SENDER_ID,
+	appId: process.env.NEXT_PUBLIC_REACT_APP_APP_ID,
+	measurementId: process.env.NEXT_PUBLIC_REACT_APP_MEASUREMENT_ID,
+});
 const db = getFirestore(app);
 const auth = getAuth(app);
 
---
+++ components/Plate/Plate.js
@@ -0,0 +1,2 @@
+import React from "react";
+import { Plate } from "@udecode/plate";
